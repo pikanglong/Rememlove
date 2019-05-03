825c0aa9176e0ef221be86fd63b67e5ee9c82ef2
X3ZvaWQ <x3zvawq@gmail.com>
2019-05-04 04:34:46 +0800

    init database

---
+++ database/migrations/2019_05_04_041956_create_membox_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemboxTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membox', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('binding_id');
+            $table->string('title');
+            $table->string('contents');
+            $table->string('img')->comment('储存图片的位置,多张图片用|分隔')->nullable();
+            $table->date('time_see')->comment('允许查看的时间')->nullable();
+            $table->string('password')->nullable();
+            $table->string('tips')->nullable();
+            $table->string('share_link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('membox');
+    }
+}
---
+++ database/migrations/2019_05_04_042156_create_checkin_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCheckinTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checkin', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('binding_id');
+            $table->string('do');
+            $table->date('date');
+            $table->tinyInteger('complete');
+            $table->string('img')->nullable();
+            $table->string('share_link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('checkin');
+    }
+}
